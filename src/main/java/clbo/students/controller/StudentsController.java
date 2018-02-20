122e577b0699bf494d4a3f10d7a6fd0f41a914cf
clbokea <clbo@kea.dk>
2018-02-20 22:34:53 +0100

    rm import

--- src/main/java/clbo/students/controller/StudentsController.java
+++ src/main/java/clbo/students/controller/StudentsController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.print.DocFlavor;
 import java.util.ArrayList;
 import java.util.Date;
 
